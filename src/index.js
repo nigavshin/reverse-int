f8808eee4b3774afc73e853d16518e7ec3c3e38c
Никита Гавшин <git add>
2020-10-20 15:34:19 +0400

    feat: start task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        return 1 * reverse(-n);
+      }
+      const solution = (n + "").split("").reverse().join("");
+    
+      return solution;
 }
